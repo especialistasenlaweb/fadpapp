1144d8b344c0eabd6e5f4bd729a8dfbd9e5b39ed
Mario Rincon <webmaster@camaleoncreativo.com>
2016-12-03 08:55:15 -0500

    inicio
    inicio

---
+++ clase7/namespace/aplicacion.php
@@ -0,0 +1,14 @@
+<?php 
+	namespace espaciofadp;
+	include("funciones.php");
+
+	//use const espaciofadp\nombre;
+	echo nombre;
+
+	//use function espaciofadp\saludo;
+	saludo();
+
+	//use espaciofadp\prueba;
+	prueba::saludo();
+
+ ?>
---
+++ clase7/namespace/funciones.php
@@ -0,0 +1,15 @@
+<?php 
+	namespace espaciofadp;
+	const nombre="<br>MArio<br>";
+
+	function saludo(){
+		echo "<hr>Hola desde la Función<hr>";
+	}
+
+	class prueba{
+		public static function saludo(){
+			echo "<hr>Hola desde clase metodo saludo<hr>";
+		}
+	}
+
+ ?>
